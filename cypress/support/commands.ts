87b7d33c484b3785fe716552c1deff6346701217
Alejandro <166451693+Alejandro-C-A@users.noreply.github.com>
2026-03-01 22:52:42 -0500

    Initial commit: Cypress E2E tests for Demoblaze signup/login

---
+++ cypress/support/commands.ts
@@ -0,0 +1,19 @@
+declare global {
+    namespace Cypress {
+      interface Chainable {
+        signup(user: { username: string; password: string }): Chainable<any>;
+        login(user: { username: string; password: string }): Chainable<any>;
+      }
+    }
+  }
+  
+  Cypress.Commands.add("signup", (user) => {
+    return cy.request("POST", "/signup", user);
+  });
+  
+  Cypress.Commands.add("login", (user) => {
+    return cy.request("POST", "/login", user);
+  });
+  
+  export {};
+  
